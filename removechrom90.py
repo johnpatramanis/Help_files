b7d76173645a5fd632d80f9ab3f03c9b49f517fd
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2018-11-05 15:04:53 +0200

    Update removechrom90.py

--- removechrom90.py
+++ removechrom90.py
@@ -13,11 +13,13 @@ args = parser.parse_args()
 file1=open(str(args.file[0]))
 file2=open(str(args.out[0]),'w')
 
-
+mychroms=[x for x in range(0,24)]
+mychroms.append('MT')
 
 for f in file1:
-        line=f.strip().split()
-        if str(line[0])==str(90):
-		line[0]=str(0)
+	if f[0]!='#':
+        	line=f.strip().split()
+        	if str(line[0])not in mychroms:
+			line[0]=str(0)
 	file2.write("\t".join(line))
 	file2.write("\n")
