77f026c1b942e24e0ab30a6413c1cb00e0f8bc22
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2018-11-08 14:49:17 +0200

    Add files via upload

---
+++ comparegvcf.py
@@ -0,0 +1,48 @@
+import re
+import argparse
+
+parser = argparse.ArgumentParser()  
+parser.add_argument('--gvcf',nargs=1,type=str)
+parser.add_argument('--bim',nargs=1,type=str)
+
+args = parser.parse_args()
+
+
+
+
+file1=open(str(args.gvcf[0]))
+file2=open(str(args.bim[0]))
+newfilename='new{}'.format(file1)
+newfile=open(str(newfilename),'w')
+
+
+data2=[]
+for f in file2:
+	line=f.split()
+	data2.append(str(line[0])+":"+str(line[3]))
+
+
+setdata2=set(data2)
+print(setdata2)
+
+
+counts=0
+for line in file1:
+    if line[0]!='#':
+        line=f.split().strip()
+        position=str(line[0])+':'+str(line[1])
+        if position in setdata2:
+            newfile.write(line)
+            newfile.write('\n')
+            counts+=1
+        else:
+            pass
+        
+    else:
+        newfile.write(line)
+        newfile.write('\n')
+
+
+
+
+
